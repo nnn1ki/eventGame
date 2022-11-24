3c05628e9c372fea7b1fd455a9647169b2ecc9cd
Your Name <you@example.com>
2022-11-24 19:43:26 +0800

    instruction is done. start maiking ind-al ex

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
+
+
 namespace eventsGame
 {
     public partial class Form1 : Form
@@ -25,6 +27,17 @@ namespace eventsGame
             InitializeComponent();
 
             player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+            player.OnOverlap += (p, obj) =>
+            {
+                txtLog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] Игрок пересекся с {obj}\n" + txtLog.Text;
+            };
+
+            player.OnMarkerOverlap += (m) =>
+            {
+                оbjects.Remove(m);
+                marker = null;
+            };
+
             marker = new Marker(pbMain.Width / 2 + 50, pbMain.Height / 2 + 50, 0);
 
             оbjects.Add(marker);
@@ -33,48 +46,79 @@ namespace eventsGame
             оbjects.Add(new MyRectangle(50, 50, 0));
             оbjects.Add(new MyRectangle(100, 100, 45));
 
-            //myRect = new MyRectangle(100, 100, 45);
         }
 
         private void pbMain_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
-
             g.Clear(Color.White);
 
+            //updatePlayer(); 
+
+            // пересчитываем пересечения
+            foreach (var obj in оbjects.ToList())
+            {
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    player.Overlap(obj);
+                    obj.Overlap(player);
+                }
+            }
+
+            // рендерим объекты
             foreach (var obj in оbjects)
             {
-                g.Transform = obj.GetTranform();
-                obj.Render(g); 
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
             }
 
 
-            //g.Transform = myRect.GetTranform();
-            //myRect.Render(g); 
-            
+
         }
+
+
+        private void updatePlayer()
+        {
+            if (marker != null)
+            {
+                float dx = marker.X - player.X;
+                float dy = marker.Y - player.Y;
+                float lenght = MathF.Sqrt(dx * dx + dy * dy);
+                dx /= lenght;
+                dy /= lenght;
+
+                player.vX += dx * 0.5f;
+                player.vY += dy * 0.5f;
+
+                player.Angle = 90 - MathF.Atan2(player.vX, player.vY) * 180 / MathF.PI;
+            }
+
+            player.vX += -player.vX + 0.1f;
+            player.vY += -player.vY * 0.1f;
+
+            player.X += player.vX;
+            player.Y += player.vY; 
+
+        }
+
+
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            //расчитаем вектор между игрокоом и меркером
-            float dx = marker.X - player.X;
-            float dy = marker.Y - player.Y;
-
-            //найдем ее длину 
-            float lenght = MathF.Sqrt(dx * dx + dy * dy);
-            dx /= lenght;
-            dy /= lenght;
-
-            player.X += dx * 2;
-            player.Y += dy * 2;
-
-            pbMain.Invalidate(); 
+            pbMain.Invalidate();
+            updatePlayer();
         }
 
-        private void Form1_MouseClick(object sender, MouseEventArgs e) {}
+        
 
         private void pbMain_MouseClick(object sender, MouseEventArgs e)
         {
+            if (marker == null)
+            {
+                marker = new Marker(0, 0, 0);
+                оbjects.Add(marker);
+            }
+
             marker.X = e.X;
             marker.Y = e.Y;
         }
--- Objects/Marker.cs
+++ Objects/Marker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Text;
 
 namespace eventsGame.Objects
@@ -9,6 +10,7 @@ namespace eventsGame.Objects
     {
         public Marker(float x, float y, float angle) : base(x, y, angle)
         {
+
         }
 
 
@@ -17,6 +19,13 @@ namespace eventsGame.Objects
             g.FillEllipse(new SolidBrush(Color.Red), -3, -3, 6, 6);
             g.DrawEllipse(new Pen(Color.Red), -6, -6, 12, 12);
             g.DrawEllipse(new Pen(Color.Red), -10, -10, 20, 20);
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var parth = base.GetGraphicsPath();
+            parth.AddEllipse(-3, -3, 6, 6);
+            return parth; 
         }
 
     }
